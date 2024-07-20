e68a76622f7ca3f5d80a8fa425bd117772318e87
MikeHe2 <michael.he30@hotmail.com>
2024-07-19 21:54:19 -0400

    ADD: add task 3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this.code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new Error('Code must be a string');
+    }
+  }
+
+  get name() {
+    return this.code;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
