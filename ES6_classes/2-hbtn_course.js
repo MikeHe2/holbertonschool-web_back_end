fbf425ca3c33e92bc967e0483cbcbc70db35d259
MikeHe2 <michael.he30@hotmail.com>
2024-07-19 21:11:47 -0400

    ADD: task 2 and fix eslint

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+class HolbertonCourse {
+  constructor(name, lenght, students) {
+    this._name = name;
+    this._lenght = lenght;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name should be a string');
+    }
+    this._name = newName;
+  }
+
+  get lenght() {
+    return this.lenght;
+  }
+
+  set lenght(newLengght) {
+    if (typeof newLengght !== 'number') {
+      throw new TypeError('Lenght should be a number');
+    }
+    this._lenght = newLengght;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents)) {
+      throw new TypeError('Students should be an array');
+    }
+    this._students = newStudents;
+  }
+}
+
+export default HolbertonCourse;
