6247366875786f909927badeaf3c038b38b9f4a0
MikeHe2 <michael.he30@hotmail.com>
2024-07-23 19:02:13 -0400

    ADD: add task 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const food = new Map([
+    ['apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return food;
+}
