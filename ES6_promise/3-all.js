06f6fd460c44d47e598f9f86ff9d394835301b8d
MikeHe2 <michael.he30@hotmail.com>
2024-07-24 16:35:02 -0400

    FIX: fix output

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,8 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      const photo = results[0];
-      const user = results[1];
+      const [photo, user] = results;
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
