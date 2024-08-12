848daaed9f4c375b5af6e47e45cccd080900a981
MikeHe2 <michael.he30@hotmail.com>
2024-08-12 16:46:39 -0400

    Add floor function with type annotations

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This is a task
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Calculate the floor value of a given number.
+
+    Args:
+        n (float): The number to calculate the floor value for.
+
+    Returns:
+        float: The floor value of the given number.
+    """
+
+    return int(n)
