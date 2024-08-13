79fe2435e637dd59fbfcf222fd683a65d3a3693c
MikeHe2 <michael.he30@hotmail.com>
2024-08-13 18:53:35 -0400

    chore: Add task_wait_random function with type annotations and docstring

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+This is a task
+"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay):
+    """
+    Creates and returns a task that waits for a random amount of time.
+
+    Args:
+        max_delay (int): The maximum delay in seconds.
+
+    Returns:
+        asyncio.Task: A task that waits for a random amount of time.
+    """
+
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
