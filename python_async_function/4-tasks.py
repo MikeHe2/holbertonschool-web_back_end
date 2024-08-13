64c048a5eea96c72fcc6a14b99ab8ddc2fa61eab
MikeHe2 <michael.he30@hotmail.com>
2024-08-13 19:02:06 -0400

    chore: Add task_wait_n function to execute multiple coroutines concurrently

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Execute multiple coroutines concurrently and return their results.
 
