f073d2a80cd7a151319173bb6cde2bcbe1074991
MikeHe2 <michael.he30@hotmail.com>
2024-08-13 14:39:44 -0400

    chore: Add make_multiplier function with type annotations

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+
+    def multiply(num: float) -> float:
+        return num * multiplier
+
+    return multiply
