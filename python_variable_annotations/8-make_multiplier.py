fd2d6d25a67d1bd636bf8de3e78f5e8c489096a2
MikeHe2 <michael.he30@hotmail.com>
2024-08-13 14:51:45 -0400

    chore: Refactor make_multiplier function with type annotations and docstring

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,15 +5,18 @@ This is task
 
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
-    Returns a function that multiplies a given number by the specified multiplier.
+    Returns a function that multiplies a given number by the specified
+    multiplier.
 
     Args:
         multiplier (float): The multiplier to be used for multiplication.
 
     Returns:
-        Callable[[float], float]: A function that takes a float as input and returns the result of multiplying it by the multiplier.
+        Callable[[float], float]: A function that takes a float as input and
+        returns the result of multiplying it by the multiplier.
     """
 
     def multiply(num: float) -> float:
