0be5295683eddc3f8cac38afbe0659a840bbc142
MikeHe2 <michael.he30@hotmail.com>
2024-08-13 14:50:39 -0400

    chore: Add make_multiplier function with type annotations and docstring

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
+"""
+This is task
+"""
 
 from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a given number by the specified multiplier.
+
+    Args:
+        multiplier (float): The multiplier to be used for multiplication.
+
+    Returns:
+        Callable[[float], float]: A function that takes a float as input and returns the result of multiplying it by the multiplier.
+    """
 
     def multiply(num: float) -> float:
         return num * multiplier
