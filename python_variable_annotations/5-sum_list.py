9826d2122913d660b391a47f46e01dbddd1ec545
MikeHe2 <michael.he30@hotmail.com>
2024-08-13 13:39:09 -0400

    chore: Add sum_list function with type annotations

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def sum_list(input_list: str) -> float:
+    """
+    Calculate the sum of all elements in the input list.
+
+    Args:
+        input_list (str): The list of elements to be summed.
+
+    Returns:
+        float: The sum of all elements in the list.
+    """
+
+    return float(sum(input_list))
