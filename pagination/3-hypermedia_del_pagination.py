1ed00c00c46f684f00a63a3b802f346bdb04e98f
MikeHe2 <michael.he30@hotmail.com>
2024-08-20 21:42:55 -0400

    chore: Refactor pagination helper function for proper formatting and comments

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Retrieves a hypermedia index page from the dataset.
+
+        Args:
+            index (int, optional): The starting index of the page. Defaults to None.
+            page_size (int, optional): The maximum number of items in the page. Defaults to 10.
+
+        Returns:
+            dict: A dictionary containing the following information:
+                - 'index': The starting index of the page.
+                - 'next_index': The index of the next page.
+                - 'page_size': The number of items in the page.
+                - 'data': The items in the page.
+        """
+
+        assert 0 <= index < len(self.dataset()), "Invalid index value."
+
+        indexed_dataset = self.indexed_dataset()
+        indexed_page = {}
+
+        i = index
+        while (len(indexed_page) < page_size and i < len(self.dataset())):
+            if i in indexed_dataset:
+                indexed_page[i] = indexed_dataset[i]
+            i += 1
+
+        page = list(indexed_page.values())
+        page_indices = indexed_page.keys()
+
+        return {
+            'index': index,
+            'next_index': max(page_indices) + 1,
+            'page_size': len(page),
+            'data': page
+        }
