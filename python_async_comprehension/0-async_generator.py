3c6edcb11aaab5752773fa41adc7f6463b500d0c
MikeHe2 <michael.he30@hotmail.com>
2024-08-20 20:07:49 -0400

    chore: Add async_generator function with type annotations and docstring

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""This is a task"""
+import asyncio
+import random
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Asynchronous generator that yields random floating-point numbers.
+
+    Yields:
+        float: A random floating-point number between 0 and 10.
+
+    Raises:
+        None
+
+    Returns:
+        None
+    """
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
