7bf5ebf44201269b9dc832f15e0903cabf6156d3
MikeHe2 <michael.he30@hotmail.com>
2024-08-22 10:06:17 -0400

    chore: Refactor insert_school function to improve code readability and maintainability

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This is a task"""
+
+from pymongo import MongoClient
+
+def insert_school(mongo_collection, **kwargs):
+
+    documents = mongo_collection.insert_one(kwargs)
+
+    return documents
