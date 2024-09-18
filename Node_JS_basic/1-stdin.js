6d3fa84d10218bcd1ed83a134ca66e7ac16bb409
MikeHe2 <michael.he30@hotmail.com>
2024-09-18 16:00:25 -0400

    Refactor stdin.js to improve user input handling and display name

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\r\n');
 
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input !== null) {
     const name = input.toString().trim();
-    console.log(`Your name is: ${name}`);
+    console.log(`Your name is: ${name}\r`);
     process.stdin.end();
   }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  console.log('This important software is now closing\r');
 });
