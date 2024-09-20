2a5f4694b7623ffd25526233fc0cd2abe51eff1e
MikeHe2 <michael.he30@hotmail.com>
2024-09-19 20:30:02 -0400

    Refactor countStudents function to fix response handling

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  let dbInfo = 'This is the list of our students\n';
+
+  try {
+    const studentsInfo = await countStudents(process.argv[2]);
+    dbInfo += studentsInfo;
+    res.send(dbInfo);
+  } catch (err) {
+    dbInfo += `Error: ${err.message}`;
+    res.send(dbInfo);
+  }
+});
+
+app.listen(port);
+
+module.exports = app;
